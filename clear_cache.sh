da59ded3ff0e6ba1c802f5e1f2037e68d153b40c
JRSouth <notmyemail@jrsouth.com>
2013-06-12 11:33:21 +0100

    Update cache clearing shell script

--- clear_cache.sh
+++ clear_cache.sh
@@ -3,10 +3,17 @@ echo ".------------------------------.";
 echo "| Clearing template tool cache |";
 echo "'------------------------------'";
 echo " - Removing old files...";
-rm -rf ./cache/*;
-echo " - Recreating subfolders...";
-cd cache;
-mkdir img pdf thumbnails default upload;
-echo " - Setting permissions...";
-chmod 0777 ./*;
+#rm -rf ./cache/*;
+
+rm -rf ./cache/default/*.jpg
+rm -rf ./cache/img/*.jpg
+rm -rf ./cache/pdf/*.pdf
+rm -rf ./cache/thumbnails/*.jpg
+
+
+#echo " - Recreating subfolders...";
+#cd cache;
+#mkdir img pdf thumbnails default upload;
+#echo " - Setting permissions...";
+#chmod 0777 ./*;
 echo "Done.";
