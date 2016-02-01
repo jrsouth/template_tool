e333f5ed1114c3573243a2056598126882ee7737
jrsouth <james.south@gmail.com>
2016-02-01 18:29:30 +0100

    Actually added the database setup files this time (install.php)

---
+++ install.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * installer.php
+ *
+ * @author jrsouth (GitHub)
+ * @package template_tool
+ */
+
+// Require authentication (super simple and not particularly secure)
+
+// Code for managing php_fpm
+// Needs lines below in .htaccess
+//
+// RewriteEngine on
+// RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization},L]
+
+
+// split the user/pass parts
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+    if (strlen($_SERVER['PHP_AUTH_USER']) == 0 || strlen($_SERVER['PHP_AUTH_PW']) == 0) {
+        unset($_SERVER['PHP_AUTH_USER']);
+        unset($_SERVER['PHP_AUTH_PW']);
+    }
+}
+
+// Do the Authentication
+require('settings-core.php');
+if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != $editor_username || $_SERVER['PHP_AUTH_PW'] != $editor_password) {
+    header('WWW-Authenticate: Basic realm="Template Editor"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo('You must be authorised to access this page.');
+    exit;
+}
+unset($editor_username, $editor_password);
+
+
+// Require function and class files, and run process.php to handle input
+require 'functions.php';
+require 'functions-editor.php';
+require 'process.php';
+
+
+
+// Output XML header
+echo '<?xml version="1.0" encoding="utf-8" ?>';
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+	<title>Template Tool Database Installer</title>
+	<link rel="stylesheet" type="text/css" href="style-editor.css" />
+	<script src="functions.js" type="text/javascript"> </script>
+	<script src="functions-editor.js" type="text/javascript"> </script>
+</head>
+
+<body>
+
+
+<?php require $content_path . 'header.chunk'; ?>
+
+
+<?php // Link out to editor
+  echo '<br style="clear:both;"/><a href="editor.php">&lt;&lt; Back to the template editor</a>';
+?>
+
+<hr />
+
+
+<?php
+
+// Check for existing installation
+
+$result = mysql_query('SELECT COUNT(id) FROM colours');
+if (mysql_error()) {
+  $db_exists = 0;
+} else {
+  $db_exists = 1;
+}
+
+echo('<br /><br />');
+
+if (isset($_POST['confirm']) && (strtolower($_POST['confirm']) === 'yes')) {
+
+	// DO THE DATABASE DROP/LOAD
+        echo('<pre>');
+	// CODE STOLEN FROM http://stackoverflow.com/a/19752106
+
+	echo('Loading up a fresh database... ');
+
+	$importfile = 'install/default_database.sql';
+	// Temporary variable, used to store current query
+	$templine = '';
+	// Read in entire file
+	$lines = file($importfile);
+	// Loop through each line
+	foreach ($lines as $line)
+	{
+		// Skip it if it's a comment
+		if (substr($line, 0, 2) == '--' || $line == '')	
+		    continue;
+	
+		// Add this line to the current segment
+		$templine .= $line;
+		// If it has a semicolon at the end, it's the end of the query
+		if (substr(trim($line), -1, 1) == ';')
+		{
+		    // Perform the query
+		    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+		    // Reset temp variable to empty
+		    $templine = '';
+		}
+	}
+	 echo "Successful\n";
+
+
+       // DELETE TEMPLATE FILES (INCLUDING IMAGES AND FONTS)
+       echo('Removing any existing template files... ');
+	$files = array_merge(
+	  glob('storage/templates/*'),
+	  glob('storage/templates/default_images/*'),
+	  glob('storage/fonts/*')
+	);
+	if ($files) {
+	  foreach ($files as $file) {
+            if($file == '.gitignore') continue;
+	    if(filetype($file) == 'dir') continue;
+	    unlink($file);
+	  }
+	}
+       echo("Removed\n");
+
+
+       // COPY TEST TEMPLATE AND DEFAULT FONTS (LUCIDA SANS UNICODE / ROBOTO VARIANTS)
+       echo('Copying sample template files... ');
+
+	copy('install/test_template.pdf', 'storage/templates/000000_test_template.pdf');
+	$files = glob('install/fonts/*');
+	if ($files) {
+	  foreach ($files as $file) {
+	    copy($file, 'storage/fonts/'.basename($file));
+	  }
+	}
+
+       echo("Copied\n");
+
+
+       // CLEAR CACHES
+       echo('Clearing caches... ');
+	$files = glob('cache/*/*');
+	if ($files) {
+	  foreach ($files as $file) {
+            if($file == '.gitignore') continue;
+	    if(filetype($file) == 'dir') continue;
+	    unlink($file);
+	  }
+	}
+       echo("Cleared\n");
+
+       echo('</pre><p>Assuming no errors were reported, you should be able to test out the default template <a href="index.php">here</a>.</p>');
+
+} else {
+	echo('<div style="border: 2px solid #cc0000; padding: 1em 2em; margin: 1em 3em; background-color: #ffcccc;">');
+	if ($db_exists) {
+	    echo('<p style="font-size:1.5em;color:#cc0000;font-weight:bold;">CAUTION: The database referred to in your <strong>settings.php</strong> file ("'.$db_database.'" on '.$db_server.') already contains data. Proceeding will reset the database to its default state and will remove any templates, fonts, and colours that may be stored in it.');
+	} else {
+	    echo('<p style="font-size:1.5em;color:#cc0000;font-weight:bold;">CAUTION: The database referred to in your <strong>settings.php</strong> file ("'.$db_database.'" on '.$db_server.') appears to be empty, but please be aware that proceeding will reset the database to a default state, removing any templates, fonts, and colours that may be stored in it.');
+	}
+
+	// PRESENT OPTION TO CONFIRM PROCESS
+	echo('<hr style="border: 1px solid #cc0000;"/><form method="POST"><p style="font-size:1.2m;font-weight:bold;">Do you understand that any template data that may exist in the "'.$db_database.'" database on '.$db_server.' will be permanently removed and the database returned to a freshly-installed state?</p><input type="text" name="confirm" label="Type YES to confirm" /> <sub>(Type "YES" to confirm)</sub></p><input type="Submit" />'.(isset($_POST['confirm'])?' You must type "YES" into the input box to proceed.':'').'</form></div>');
+}
+
+    
+
+?>
+<br style="clear:both;" />
+</body>
+
+</html>
+
---
+++ install/default_database.sql
@@ -0,0 +1,199 @@
+-- Default template tool data set
+
+--
+-- Table structure for table `colours`
+--
+
+DROP TABLE IF EXISTS `colours`;
+
+CREATE TABLE `colours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `RGBA` char(8) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Default data for table `colours`
+--
+
+LOCK TABLES `colours` WRITE;
+INSERT INTO `colours` VALUES
+	(NULL,'Black','000000FF'),
+	(NULL,'Red','AA0000FF'),
+	(NULL,'Grey','56524EFF'),
+	(NULL,'Light Grey','A7A7A7FF'),
+	(NULL,'White','FFFFFFFF'),
+	(NULL,'Yellow','F8BF00FF'),
+	(NULL,'Blue','00A3ADFF'),
+	(NULL,'Purple','771AA1FF'),
+	(NULL,'Green','74BC09FF'),
+	(NULL,'Pink','DF2B5FFF');
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fields`
+--
+
+DROP TABLE IF EXISTS `fields`;
+CREATE TABLE `fields` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `template_id` int(11) NOT NULL,
+  `type` enum('normal','data','wrapper') DEFAULT 'normal',
+  `name` varchar(32) NOT NULL,
+  `default_text` text NOT NULL,
+  `force_uppercase` tinyint(4) NOT NULL DEFAULT '0',
+  `character_limit` int(11) DEFAULT NULL,
+  `font_id` int(11) NOT NULL,
+  `font_size` double DEFAULT NULL,
+  `colour_id` int(11) NOT NULL DEFAULT '1',
+  `x_position` double DEFAULT NULL,
+  `y_position` double DEFAULT NULL,
+  `wrap_width` double DEFAULT NULL,
+  `leading` double NOT NULL DEFAULT '0',
+  `parent` int(11) DEFAULT NULL,
+  `page` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `template_id` (`template_id`),
+  KEY `font_id` (`font_id`)
+);
+
+--
+-- Default data for table `fields`
+--
+
+LOCK TABLES `fields` WRITE;
+INSERT INTO `fields` VALUES
+(NULL,1,'normal','Title','Template title',1,50,6,18,8,50,70,110,20,0,1),
+(NULL,1,'normal','Main text','This is where your text goes.',0,2048,6,12,1,50,80,110,15,0,1);
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fonts`
+--
+
+DROP TABLE IF EXISTS `fonts`;
+CREATE TABLE `fonts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `display_name` varchar(64) NOT NULL,
+  `font_file` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Default data for table `fonts`
+--
+
+LOCK TABLES `fonts` WRITE;
+INSERT INTO `fonts` VALUES
+	(NULL,'Roboto Light','roboto_light.php'),
+	(NULL,'Roboto Black Italic','roboto_blackitalic.php'),
+	(NULL,'Roboto Black','roboto_black.php'),
+	(NULL,'Roboto Bold Italic','roboto_bolditalic.php'),
+	(NULL,'Roboto Bold','roboto_bold.php'),
+	(NULL,'Roboto Italic','roboto_italic.php'),
+	(NULL,'Roboto Light Italic','roboto_lightitalic.php'),
+	(NULL,'Roboto Medium Italic','roboto_mediumitalic.php'),
+	(NULL,'Roboto Medium','roboto_medium.php'),
+	(NULL,'Roboto Regular','roboto_regular.php'),
+	(NULL,'Roboto Thin Italic','roboto_thinitalic.php'),
+	(NULL,'Roboto Thin','roboto_thin.php');
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `template_id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `x_position` double NOT NULL,
+  `y_position` double NOT NULL,
+  `width` double NOT NULL,
+  `height` double NOT NULL,
+  `alignment` varchar(12) NOT NULL,
+  `page` int(11) DEFAULT '1',
+  `hide` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `template_id` (`template_id`)
+);
+
+--
+-- Default data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `saved_templates`
+--
+
+DROP TABLE IF EXISTS `saved_templates`;
+CREATE TABLE `saved_templates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(64) DEFAULT 'default',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `template_id` int(11) NOT NULL,
+  `data` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Default data for table `saved_templates`
+--
+
+LOCK TABLES `saved_templates` WRITE;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `templates`
+--
+
+DROP TABLE IF EXISTS `templates`;
+CREATE TABLE `templates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `tags` varchar(256) DEFAULT NULL,
+  `pdf_file` varchar(128) NOT NULL,
+  `permissions` varchar(32) DEFAULT NULL,
+  `bleed` double DEFAULT NULL,
+  `owner` varchar(128) DEFAULT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  `pagecount` int(11) DEFAULT '1',
+  `height` double DEFAULT '297',
+  `width` double DEFAULT '210',
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Default data for table `templates`
+--
+
+LOCK TABLES `templates` WRITE;
+INSERT INTO `templates` VALUES
+	(1,'Sample template','sample','000000_test_template.pdf','',0,'',1,1,297,210);
+UNLOCK TABLES;
+
+--
+-- Table structure for table `working_templates`
+--
+
+DROP TABLE IF EXISTS `working_templates`;
+CREATE TABLE `working_templates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `template_id` int(11) NOT NULL,
+  `data` text,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Default data for table `working_templates`
+--
+
+LOCK TABLES `working_templates` WRITE;
+UNLOCK TABLES;
+
