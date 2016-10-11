b219b02726a9063d031c5601d1f2ab98ce4e51ec
jrsouth <james.south@gmail.com>
2016-10-11 09:58:08 +0100

    More symfony structure

--- symfony/src/AppBundle/Controller/CreateController.php
+++ symfony/src/AppBundle/Controller/CreateController.php
@@ -9,12 +9,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class CreateController extends Controller
 {
     /**
+     * @Route("/", name="create-template")
      * @Route("/create/{id}/{downloadName}", name="create-template", requirements={"id": "\d+"})
      */
     public function numberAction($id = 0, $downloadName = null)
     {
 
         if ($id === 0) {
+            return($this->render('default/index.html.twig'));
+        } else {
+            // Set up form
+            // or create parameters
             return($this->render('create/select.html.twig'));
         }
 
