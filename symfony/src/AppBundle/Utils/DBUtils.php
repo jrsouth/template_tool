de32ba11bff2cf79ca280cfcc4a95628e7809328
jrsouth <james.south@gmail.com>
2016-10-11 10:29:05 +0100

    Storage setup for symfony

---
+++ symfony/src/AppBundle/Utils/DBUtils.php
@@ -0,0 +1,10 @@
+<?php
+namespace AppBundle\Utils;
+
+class DBUtils
+{
+    public function getColors()
+    {
+        return([['id'=>1,'name'=>'One','RGBA'=>'ff3333ff'],['id'=>2,'name'=>'Two','RGBA'=>'33ff33ff'],['id'=>3,'name'=>'Three','RGBA'=>'3333ffff']]);
+    }
+}
